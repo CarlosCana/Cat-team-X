ef385b76b55fd78f1ac6e2c7d42d47375296163e
Carlos CANA <carloscana@gmail.com>
2018-08-30 11:15:45 +0200

    comment some lines for push in heroku

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,8 @@ ActiveRecord::Schema.define(version: 2018_08_29_080529) do
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.decimal "price", precision: 3, scale: 2
+    # In comment for test in heroku, precision: 3, scale: 2
+    t.decimal "price"
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
